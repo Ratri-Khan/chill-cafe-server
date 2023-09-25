09ef2829196162268c8063a1d4bec8cc1af05f0e
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-09-25 13:28:08 +0600

    recipe details data sent complete

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ const detailsInfo = require('./data/Information.json');
 
 app.get('/chefInfo',(req,res)=>{
     res.send(chefInfo);
-});
+})
 app.get('/detailsInfo',(req,res)=>{
   res.send(detailsInfo);
 })
+
+app.get('/detailsInfo/:id', (req,res) =>{
+  const id = req.params.id;
+  console.log(id);
+  const selectedInfo = detailsInfo.find(i => parseInt(i.id) === id);
+  res.send(selectedInfo);
+})
+app.get('/chefInfo/:id',(req,res) =>{
+  const id = parseInt(req.params.id);
+  console.log(id);
+  const informationChefs = detailsInfo.filter(i => parseInt(i.id) === id);
+  res.send(informationChefs);
+})
+
 
 
 
